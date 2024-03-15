bf76076fd1568da33c2109dbd18bda89fb975cf6
AIWalaBro <aiwalabro@gmail.com>
2024-03-15 14:41:05 +0530

    constant file updated

---
+++ Customer_Support_Integration/app.py
@@ -0,0 +1,4 @@
+import streamlit as st
+from utils import *
+import os
+from constants import *
--- Customer_Support_Integration/constants.py
+++ Customer_Support_Integration/constants.py
@@ -1,3 +1,3 @@
 WEBSITE_URL="https://jobs.excelcult.com/wp-sitemap-posts-post-1.xml"
 PINECONE_ENVIRONMENT="gcp-starter"
-PINECONE_INDEX="demo"
+PINECONE_INDEX="bot"
