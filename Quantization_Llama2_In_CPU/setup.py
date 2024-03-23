24296d6cbb3d69e90f4b21aaae41c5732b9953be
AIWalaBro <aiwalabro@gmail.com>
2024-03-23 11:37:29 +0530

    setup file added

---
+++ Quantization_Llama2_In_CPU/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Bharatbhushan',
+    author_email= 'aiwalabro@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
